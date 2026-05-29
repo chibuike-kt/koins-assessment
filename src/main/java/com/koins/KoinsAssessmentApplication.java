c4ce300a37c59ad77da7bf81ce1df00ce2cf4334
Kingsley <kingsleychibueze16@gmail.com>
2026-05-29 02:09:47 +0100

    chore: initial Spring Boot 3.5.0 project scaffold

---
+++ src/main/java/com/koins/KoinsAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.koins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoinsAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoinsAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koins/KoinsAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoinsAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
